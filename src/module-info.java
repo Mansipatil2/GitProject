2139b5e20b1f28ec069b6bc0dd7f24eace5046dd
Mansi <mansipriya2002@gmail.com>
2023-12-26 13:00:15 +0100

    added git eclipse project into git

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Makarand
+ *
+ */
+module GitProject {
+}
---
+++ src/myPack/class1.java
@@ -0,0 +1,18 @@
+package myPack;
+
+public class class1 
+{
+	public void addition(int a,int b)
+	{
+		int c= a+b;
+		System.out.println("Addition is " +c);
+	}
+	
+	public static void main(String[] args) 
+	{
+		class1 obj =new class1();
+		obj.addition(10, 10);
+		
+	 		
+	}
+}
